a71fbf78e0b502c0d9750ba2ce9637844f378125
Your Name <AriK@ari-kobrens-macbook.local>
2009-05-31 22:52:53 -0400

    change to models.py

--- needletail/login/models.py
+++ needletail/login/models.py
@@ -4,16 +4,11 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 
 class Band(models.Model):
-    name = models.CharField(max_length = 500)
+    name     = models.CharField(max_length = 500)
+    genre    = models.CharField(max_length = 50 ) 
+    location = models.CharField(max_length = 200)
+    bio      = models.CharField()
 
-#class Band_Member(models.Model):
-#    first     = models.CharField( max_length = 100)
-#    last      = models.CharField( max_length = 100)
-#    username  = models.CharField( max_length = 100)
-#    password  = models.CharField( max_length = 100)
-#    email     = models.EmailField(max_length = 100)
-#    bands     = models.ManyToManyField(Band)
-    
 
 class User_Profile(models.Model):
     user  = models.OneToOneField(User)
@@ -21,4 +16,4 @@ class User_Profile(models.Model):
 
 
 admin.site.register(Band)
-#admin.site.register(Band_Member)
+
